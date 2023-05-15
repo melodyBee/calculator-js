e0ccf98758386d267a0b70f2389591af79cf79a7
melodyBee <abeerafatimarub@gmail.com>
2023-05-15 19:36:15 +0500

    putting logic in calculator

---
+++ script/app.js
@@ -0,0 +1,13 @@
+function getnum(num){
+    document.getElementById('result').value += num;
+}
+function clear(){
+    document.getElementById('result').value = '';
+}
+function remove(){
+    var res = document.getElementById('result').value;
+    document.getElementById('result').value =document.getElementById('result').value.slice(0,res.length-1);
+}
+function result(){
+    document.getElementById('result').value = eval(document.getElementById('result').value)
+}
